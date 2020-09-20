13843ef1f2423face5682aa85cf6288b9e12b9bf
chrisisler <christopherisler1@gmail.com>
2020-09-20 15:57:30 -0400

    Create react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Firegram 🚀</h1>
+      {/** Header */}
+      {/** Posts */}
+    </div>
+  );
+}
